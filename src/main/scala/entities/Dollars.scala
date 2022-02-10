8d01d400c35822733193775bf17c90a602057407
Omer Farooq Ahmed <omer.f.ahmed@gsk.com>
2022-02-10 16:25:05 +0000

    added ordered trait to Dollars

--- src/main/scala/entities/Dollars.scala
+++ src/main/scala/entities/Dollars.scala
@@ -1,15 +1,17 @@
 package main.scala.entities
 
 object Dollars {
+  val Zero = new Dollars(0)
   def apply(a: Int): Dollars = new Dollars(a)
 }
 
-class Dollars(val amount: Int) extends AnyVal {
+class Dollars(val amount: Int) extends AnyVal with Ordered[Dollars]{
+
+  override def compare(that: Dollars): Int = amount - that.amount
+
   def +(value: Dollars): Dollars = new Dollars(amount + value.amount)
 
   def -(value: Dollars): Dollars = new Dollars(amount - value.amount)
-
-  def >(value: Dollars): Boolean = amount > value.amount
 
   override def toString: String = "$" + amount
 
