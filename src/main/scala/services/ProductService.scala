1bd93c2e98c23a09e1782ce38cdaef727ab51870
Omer Farooq Ahmed <omer.f.ahmed@gsk.com>
2022-02-11 23:04:43 +0000

    added product service

---
+++ src/main/scala/services/ProductService.scala
@@ -0,0 +1,24 @@
+package main.scala.services
+
+import java.util.UUID
+
+import main.scala.entities._
+
+trait ProductService extends ProductsDb {
+  def addNewDepositProduct(name: String, minBalance: Int, ratePerYear: Double,
+                           transactionsAllowedPerMonth: Int = 2): UUID = {
+    val product = name match {
+      case "CoreChecking" => new CoreChecking(Dollars(minBalance), ratePerYear)
+      case "StudentChecking" => new StudentChecking(Dollars(minBalance), ratePerYear)
+      case "RewardsSavings" => new RewardsSavings(Dollars(minBalance), ratePerYear, transactionsAllowedPerMonth)
+    }
+    saveDepositProduct(product)
+    product.id
+  }
+
+  def addNewLendingProduct(annualFee: Double, apr: Double, rewardsPercent: Double): UUID = {
+    val product = new CreditCard(annualFee, apr, rewardsPercent)
+    saveLendingProduct(product)
+    product.id
+  }
+}
